4208dcff195910df7fddb2b28dd2e8f5c55c7a5c
ddafa <ddafa@EUZU-ROG_FLOW>
2024-05-29 15:04:55 +0900

    초기 커밋.

---
+++ arlem_app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace arlem_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ arlem_app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace arlem_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
